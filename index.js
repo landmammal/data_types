5102da461c76a5d0fc55bee13cd83765835bcf11
landmammal <evolpirate@gmail.com>
2021-11-16 22:36:10 -0500

    adding data type exercise

---
+++ index.js
@@ -0,0 +1,12 @@
+// console log a data type of STRING of why you wanna learn how to code.
+console.log();
+
+// console log a data type of NUMBER representing your best friends age.
+console.log();
+
+// console log a data type of BOOLEAN representing if you wanna go to mars.
+console.log();
+
+
+// BONUS: console log a data type of OBJECT, the object must be empty. (google it)
+console.log();
